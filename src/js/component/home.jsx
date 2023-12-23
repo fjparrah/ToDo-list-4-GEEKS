97ef6caf56d5e2f0df3bee44273b76c893bae1eb
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-23 11:35:47 +0000

    avance 2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-
-//create your first component
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+  const addTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
     <>
-      <div className="container  ">
-        <div className=" row mt-5 ">
+      <div className="container col-sm-12 col-lg-6  ">
+        <div className="row mt-5">
           <div className="container">
-            <h1>To-doList </h1>
+            <h1>To-do List </h1>
           </div>
           <div className="container mt-3">
-            <ul className="">
+            <ul>
               <li>
                 <input
                   placeholder="¿Qué necesitas hacer?"
@@ -23,31 +31,44 @@ const Home = () => {
                   value={inputValue}
                   onKeyUp={(e) => {
                     if (e.key === "Enter") {
-                      setInputValue("");
-                      setTodos(todos.concat(inputValue));
+                      addTodo();
                     }
                   }}
                   type="text"
                 />
               </li>
 
-              {todos.map((item, index) => (
-                <li>
-                  {item}{" "}
-                  <i
-                    class="fas fa-trash-alt"
-                    onClick={() =>
-                      setTodos(
-                        todos.filter((t, currentIndex) => index != currentIndex)
-                      )
-                    }
-                  ></i>
-                </li>
-              ))}
+              {todos.length === 0 ? (
+                <li>No hay tareas, añadir tareas</li>
+              ) : (
+                todos.map((item, index) => (
+                  <li key={index}>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <span>{item}</span>{" "}
+                      <i
+                        className="fas fa-trash-alt"
+                        onClick={() => deleteTodo(index)}
+                      ></i>
+                    </div>
+                  </li>
+                ))
+              )}
             </ul>
           </div>
           <div className="container pt-3">
-            <h3> {todos.length} tasks </h3>
+            <h3>
+              {todos.length === 0
+                ? ""
+                : todos.length === 1
+                ? `${todos.length} tarea`
+                : `${todos.length} tareas`}
+            </h3>
           </div>
         </div>
       </div>
